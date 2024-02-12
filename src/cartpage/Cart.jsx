418973aab188e2df174d340f6f1a4ccc5e54310a
Kənan Muradov <maljerov.1999@gmail.com>
2024-02-12 18:26:10 +0300

    fixes part 6

--- src/cartpage/Cart.jsx
+++ src/cartpage/Cart.jsx
@@ -29,10 +29,11 @@ function Cart() {
     <div className="md:px-20 lg:px-28">
       <Navbar></Navbar>
       <div className="mx-10 lg:mx-0">
-        <h1 className="text-5xl font-semibold mt-[80px] mb-[40px]">CART</h1>
+              <h1 className="text-5xl font-semibold mt-[80px] mb-[40px]">CART</h1>
+              <h2 className="my-3 text-xl">No Products Added</h2>
         <div className="border border-zinc-200 w-full mt-[75px]"></div>
         <div>
-          {groupedOrders.length ? groupedOrders.map((productInfo) => (<ProductInfo id={productInfo.id} count={productInfo.count} key={productInfo.id} />)) : <h2 className="my-3 text-xl">No Products Added</h2>}
+          {groupedOrders.length ? groupedOrders.map((productInfo) => (<ProductInfo id={productInfo.id} count={productInfo.count} key={productInfo.id} />)) : null}
         </div>
         <div className="flex flex-col text-2xl font-light">
           <label>Tax 18%: <span className="ml-2 font-bold"><span>$</span>{((sum / 100) * 18).toFixed(2)}</span></label>
